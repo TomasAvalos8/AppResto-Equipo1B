3800449125167715f711ef79cb0c137c47e111b9
TomasAvalos8 <tomasgonzaloavalos@gmail.com>
2024-11-18 01:05:54 -0300

    Modificacion dentro de la base de datos y algunas validaciones en los modales

--- Productos.aspx.cs
+++ Productos.aspx.cs
@@ -11,9 +11,11 @@ namespace Resto
 {
     public partial class Productos : System.Web.UI.Page
     {
+        public bool seleccionado { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
+           seleccionado = false;
             ProductoNegocio negocio = new ProductoNegocio();
             dgvProductos.DataSource = negocio.listar();
             dgvProductos.DataBind();
@@ -49,9 +51,11 @@ namespace Resto
             }
         }
 
+
         protected void CargarTXT(object sender, EventArgs e)
         {
 
+            seleccionado = true;
             GridViewRow SelectedRow = dgvProductos.SelectedRow;
 
             txtMId.Text = SelectedRow.Cells[1].Text;
@@ -67,5 +71,35 @@ namespace Resto
             dgvProductos.DataBind();
         }
 
+        protected void btnModificar_Click(object sender, EventArgs e)
+        {
+            Producto nuevo = new Producto();
+            ProductoNegocio negocio = new ProductoNegocio();
+            try
+            {
+                nuevo.Nombre = txtMNombre.Text;
+                nuevo.Descripcion = txtMDesc.Text;
+                nuevo.Precio = decimal.Parse(txtMPrecio.Text);
+                nuevo.Id=int.Parse(txtMId.Text);
+
+                negocio.modificarConSP(nuevo);
+
+
+                Response.Redirect("Productos.aspx", false);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = ex.Message.Replace("'", "\\'");
+                string script = $"alert('Ocurrio un error: necesita cargar datos para aceptar' );";
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "errorAlert", script, true);
+
+            }
+           
+        }
+
+        protected void btnAceptarEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
